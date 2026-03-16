367b22bb7f5b183146897fceb6a0579891587010
Viren Soni <71062471+VirenSoni-Dev@users.noreply.github.com>
2026-03-16 21:36:58 +0530

    from SSL to TSL in mailService.js

--- backend/services/mailService.js
+++ backend/services/mailService.js
@@ -10,7 +10,9 @@ dotenv.config();
 // ── Create reusable transporter ──
 // Created once, reused for every email sent
 const transporter = nodemailer.createTransport({
-   service: 'gmail',
+   host: 'smtp.gmail.com',
+   port: 587,
+   secure: false,
    auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
